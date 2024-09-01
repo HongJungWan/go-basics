08f1558f5a3bc1c1230ff538775fe357bda606ad
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-09-01 19:21:07 +0900

    docs: error
    - error return
    - custom error return
    - error type
    - error wrap

---
+++ error/error/wrap.go
@@ -0,0 +1,21 @@
+package main
+
+import "bufio"
+
+func MultipleFromString(str string) (int, error) {
+
+	return 0, nil
+}
+
+func readNextInt(scanner *bufio.Scanner) (int, int, error) {
+
+	return 0, 0, nil
+}
+
+func readEq(eq string) {
+
+}
+
+func main() {
+
+}
