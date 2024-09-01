d822fcec8ad8f144a0e441e7226be97d0351243a
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-09-01 20:42:06 +0900

    docs: error
    - error wrap
    - panic
    - recover

--- error/error/wrap.go
+++ error/error/wrap.go
@@ -1,21 +1,63 @@
 package main
 
-import "bufio"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func MultipleFromString(str string) (int, error) {
+	scanner := bufio.NewScanner(strings.NewReader(str))
+	scanner.Split(bufio.ScanWords) // 한 단어씩 끊어 읽기
 
-	return 0, nil
+	pos := 0
+	a, n, err := readNextInt(scanner)
+	if err != nil {
+		return 0, fmt.Errorf("Failed to readNexInt(), pos:%d err:%w", pos, err)
+	}
+
+	pos += n + 1
+	b, n, err := readNextInt(scanner)
+	if err != nil {
+		return 0, fmt.Errorf("Failed to readNexInt(), pos:%d err:%w", pos, err)
+	}
+
+	return a * b, nil
 }
 
 func readNextInt(scanner *bufio.Scanner) (int, int, error) {
+	if !scanner.Scan() {
+		return 0, 0, fmt.Errorf("Failed to Scan")
+	}
 
-	return 0, 0, nil
+	word := scanner.Text()
+	number, err := strconv.Atoi(word)
+	if err != nil {
+
+		// 에러 감싸기
+		return 0, 0, fmt.Errorf("Failed to convert word to int, word:%s err:%w", word, err)
+	}
+	return number, len(word), nil
 }
 
 func readEq(eq string) {
+	rst, err := MultipleFromString(eq)
+	if err == nil {
+		fmt.Println(rst)
+	} else {
+		fmt.Println(err)
+		var numError *strconv.NumError
 
+		// 감싸진 에러가 NumError인지 확인
+		if errors.As(err, &numError) {
+			fmt.Println("NumberError:", numError)
+		}
+	}
 }
 
 func main() {
-
+	readEq("123 3")
+	readEq("123 abc")
 }
